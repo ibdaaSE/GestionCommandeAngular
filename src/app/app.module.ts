791416978d86eafa08ebaa7f26218a4181459493
Younes <yoonux@gmail.com>
2017-11-02 13:09:09 +0100

    zer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatButtonModule, MatTooltipModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +15,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatButtonModule, MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
